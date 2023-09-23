c0b4324c8013182f146d4861abcfa43e17075371
Jasmitsingh01 <97171634+Jasmitsingh01@users.noreply.github.com>
2023-09-24 01:41:06 +0530

    delete files

--- test.js
+++
@@ -1,5 +0,0 @@
-const  { v4: uuidv4 }=require('uuid'); 
-const jwt = require('jsonwebtoken')
-const Decode=jwt.sign("Name:Jasmit","Sumit");
-const Data= jwt.verify("eyJhbGciOiJIUzI1NiJ9.TmFtZTpKYXNtaXQ.uWFumhdBM3YfhdgemVTtwvNrdQAsH768rXUJtKFc0So","Sumit");
-console.log(Data);
